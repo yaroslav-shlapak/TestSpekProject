afaec0cc69c843124e91e254c2c3f5b22048fd7a
yaroslav <yaroslav.shlapak@gmail.com>
2018-11-30 13:03:46 +0200

    initial commit

---
+++ app/src/test/java/com/example/testspekproject/packagetoruntests/pakage1/ExampleUnitTest3.kt
@@ -0,0 +1,17 @@
+package com.example.testspekproject.packagetoruntests.pakage1
+
+import io.kotlintest.shouldBe
+import org.spekframework.spek2.Spek
+
+/**
+ * Example local unit test, which will execute on the development machine (host).
+ *
+ * See [testing documentation](http://d.android.com/tools/testing).
+ */
+object ExampleUnitTest3 : Spek({
+    group("dummy test") {
+        test("addition is correct") {
+            4 shouldBe 2 * 2
+        }
+    }
+})
